6bf7d3777ab246d9cbb16e41abb2d5f8c01869e6
HeYuJun <1148370650@qq.com>
2024-08-05 12:36:36 +0800

    更新了城市结果搜索

--- week3/js/mobileVersion.js
+++ week3/js/mobileVersion.js
@@ -2,10 +2,11 @@ new Vue({
     el:'#app',
     data:{
         searchCity:'',//搜索的城市
-        currentCity:'',//当前城市
-        currentTime:'',//当前时间
-        currentDate:'',//当前日期
+        currentCity:'深圳',//当前城市
+        currentTime:'11:47',//当前时间
+        currentDate:'2024年8月5日',//当前日期
         cityMain:{  //城市详细天气数据(最高温、最低温、日出时间、日落时间、湿度、气压、风速、紫外线、天气类型)
+            nowTemp:'24°C',
             maxTemp:'100°C',
             minTemp:'0°C',
             sunRise:'8:00',
@@ -64,46 +65,45 @@ new Vue({
                 time:'12:00',//三小时之后
                 averageTemp:'21°C',
                 weatherTypeIcon:'qi-100',
-                windDirectionIcon:'southWesternWind',
+                windDirectionIcon:'北风',
                 windSpeed:'3km/h'
             },
             {
                 time:'15:00',//六小时之后
                 averageTemp:'21°C',
                 weatherTypeIcon:'qi-100',
-                windDirectionIcon:'southWesternWind',
+                windDirectionIcon:'北风',
                 windSpeed:'3km/h'
             },
             {
                 time:'18:00',//九小时之后
                 averageTemp:'21°C',
                 weatherTypeIcon:'qi-100',
-                windDirectionIcon:'southWesternWind',
+                windDirectionIcon:'北风',
                 windSpeed:'3km/h'
             },
             {
                 time:'21:00',//十二小时之后
                 averageTemp:'21°C',
                 weatherTypeIcon:'qi-100',
-                windDirectionIcon:'southWesternWind',
+                windDirectionIcon:'北风',
                 windSpeed:'3km/h'
             },
         ]
     },
     methods:{   //和风天气用户key:b38053db3dee4ae39586a04fbff4ff1c
-        searchWeather(){
-            
-        },
         getWeatherMain(){
             let locationId=0;
+            const mykey='b38053db3dee4ae39586a04fbff4ff1c'
+            let that=this;
             axios({
                 method:'get',
                 url:'https://geoapi.qweather.com/v2/city/lookup',//根据城市名字查询城市id
                 params:{
-                    location:'罗湖',     //无结果是404
-                    key:'b38053db3dee4ae39586a04fbff4ff1c'
+                    location:this.searchCity,     //无结果是404
+                    key:mykey
                 }
-            }).then(function(respose){
+            }).then((respose)=>{//用箭头函数可以使下文this指向上文
                 console.log(this.searchCity)
                 console.log(respose.data)
                 let data=respose.data;
@@ -115,14 +115,118 @@ new Vue({
                     for(let i in data.location){
                         if(data.location[i].name===this.searchCity){
                             console.log('搜寻成功,id为'+data.location[i].id)
+                            this.currentCity=this.searchCity;
                             locationId=data.location[i].id;//找到城市或区id之后查找详细天气状况
                         }
                     }
                     if(locationId==0){
-                        alert('找不到这个地方')
+                        alert('请输入正确的城市或城区名字')
+                    }
+                    else{//获取到locationId则继续寻找
+                        //获取当天天气以及未来五天天气状况
+                        axios({
+                            methods:'get',
+                            url:'https://devapi.qweather.com/v7/weather/now',
+                            params:{
+                                key:mykey,
+                                location:locationId
+                            }
+                        }).then((respose)=>{
+                            console.log('实时天气')
+                            console.log(respose.data)
+                            
+                            if(respose.data.code!=200){
+                                alert('请求错误，获取当天天气失败')
+                            }
+                            else{
+                                //实时时间变更
+                                this.currentDate=respose.data.updateTime.substr(0,4)+'年'+respose.data.updateTime.substr(5,2)+'月'+respose.data.updateTime.substr(8,2)+'日'
+                                this.currentTime=respose.data.updateTime.substr(11,5)
+                                //更新今日天气
+                                let nowData=respose.data.now
+                                // this.cityMain.maxTemp=nowData[0].tempMax;//最高温度
+                                // this.cityMain.minTemp=nowData[0].tempMax;//最低温度
+                                this.cityMain.nowTemp=nowData.temp+'°C';//当前温度温度
+                                // this.cityMain.sunRise=nowData[0].tempMax;//日出
+                                // this.cityMain.sunSet=nowData[0].tempMax;//日落
+                                this.cityMain.humidity=nowData.humidity+'%';//湿度
+                                this.cityMain.pressure=nowData.pressure+'hPa';//大气压
+                                this.cityMain.windSpeed=nowData.windSpeed+'km/h';//风速
+                                this.cityMain.weatherType=nowData.text;//当前天气类型
+                                this.cityMain.weatherTypeIcon='qi-'+nowData.icon;//天气类型图标
+                                //更新当天以及未来五天天气
+                                axios({
+                                    methods:'get',
+                                    url:'https://devapi.qweather.com/v7/weather/7d',
+                                    params:{
+                                        key:mykey,
+                                        location:locationId
+                                    }
+                                }).then((respose)=>{
+                                    console.log('未来几天天气')
+                                    console.log(respose.data)
+                                    if(respose.data.code!=200){
+                                        alert('获取未来五天天气失败')
+                                    }
+                                    else{//获取未来几天天气
+                                        let dailyData=respose.data.daily;
+                                        this.cityMain.maxTemp=dailyData[0].tempMax;//最高温度
+                                        this.cityMain.minTemp=dailyData[0].tempMin;//最低温度
+                                        this.cityMain.sunRise=dailyData[0].sunrise;//日出
+                                        this.cityMain.sunSet=dailyData[0].sunset;//日落
+                                        this.cityMain.uv=dailyData[0].uvIndex;//紫外线指数
+                                        this.fiveDayLike=[];
+                                        const weekArray = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+                                        for(let i=1;i<=5;i++)//未来五天粗略天气
+                                        {  
+
+                                           let newDailyData={
+                                            todayBefore:i,
+                                            dayOfWeekCH:weekArray[new Date(dailyData[i].fxDate).getDay()],//星期几
+                                            averageTemp:(parseInt(dailyData[i].tempMax)+parseInt(dailyData[i].tempMin))/2+'°C',//平均温度
+                                            weatherIcon:'qi-'+dailyData[i].iconDay
+                                           }
+                                           this.fiveDayLike.push(newDailyData);
+                                        }
+                                        //获取小时天气信息
+                                        axios({
+                                            methods:'get',
+                                            url:'https://devapi.qweather.com/v7/weather/24h',
+                                            params:{
+                                                key:mykey,
+                                                location:locationId
+                                            }
+                                        }).then((respose)=>{
+                                            console.log('小时天气')
+                                            console.log(respose.data)
+                                            if(respose.data.code!=200){
+                                                alert('获取小时天气失败')
+                                            }
+                                            else{
+                                                this.hourlyLike=[];
+                                                let horlyData=respose.data.hourly;
+                                                for(let i=2;i<=11;i+=3){
+                                                    let newHourlyData={
+                                                        time:horlyData[i].fxTime.substr(11,5),
+                                                        averageTemp:horlyData[i].temp+'°C',
+                                                        weatherTypeIcon:'qi-'+horlyData[i].icon,
+                                                        windDirectionIcon:horlyData[i].windDir,
+                                                        windSpeed:horlyData[i].windSpeed+'km/h'
+                                                    }
+                                                    this.hourlyLike.push(newHourlyData)
+                                                }
+                                                console.log(this.hourlyLike)
+                                            }
+                                        })
+                                    }
+                                })
+                            }
+                        })
                     }
                 }
             })
+            console.log('完毕')
+            console.log(this.hourlyLike)
         },
     },
     }
---
+++ week3/testThree.js
@@ -0,0 +1,2 @@
+let str="2021-02-16T15:00+08:00";
+console.log(str.substr(11,5))
